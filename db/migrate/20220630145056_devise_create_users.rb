0cbb3a4efb440f8ecf41eb1284c2e95679738688
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-07-01 11:38:48 +0900

    rails_admin インストール

--- db/migrate/20220630145056_devise_create_users.rb
+++ db/migrate/20220630145056_devise_create_users.rb
@@ -7,6 +7,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :name, null: false
+      t.boolean :can_edit, null: false, default: false
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -32,8 +35,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name, null: false
-      t.boolean :can_edit, null: false, default: false
       t.timestamps null: false
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,11 @@ ActiveRecord::Schema.define(version: 2022_06_30_145056) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.boolean "can_edit", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", null: false
-    t.boolean "can_edit", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
