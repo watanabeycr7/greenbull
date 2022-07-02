413367d22bee9016935f7ea8d8b1bba2c50eda2d
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-07-03 02:56:15 +0900

    ユーザー新規登録機能実装
    ユーザーグループ中間テーブルに同時に保存可能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   root to: 'schedules#index'
 end
