97ef92a0113ffddc0c5dbac3846cfa01264e34c0
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-07-02 21:51:29 +0900

    新規登録でグループを入力しないよう修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :new_registration_form, only: [:new], if: -> { controller_name == "registrations" }
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :group])
-  end
-
-  def new_registration_form
-    @registration_form = UserRegistrationForm.new
   end
 
 end
