5dddb20f67fd612c5555e413185753fb7df9d6ea
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-07-04 23:08:43 +0900

    ユーザー一覧表示
    所属グループごとに分けて表示。
    編集権限のないユーザーのみ表示。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
 
   def index
+
+    #編集権限のないユーザーを取得
     @ordinaly_users = User.where(can_edit: false)
+
+    #編集権限のあるユーザーを取得
+    @editors = User.where(can_edit: true)
+
+    #編集権限がなく、ユーザーグループ中間テーブルにIDが存在するユーザーを取得
+    @users_in_group = @ordinaly_users.where(id: UserGroup.select(:user_id))
+    
+    #編集権限がなく、ユーザーグループ中間テーブルにもIDが存在しないユーザーを取得
+    @users_not_in_group = @ordinaly_users.where.not(id: @users_in_group.ids)
+
     @groups = Group.all
   end
+
   def show
     @user = User.find(params[:id])
   end
