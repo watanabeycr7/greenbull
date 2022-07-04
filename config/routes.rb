2ed5931f1169417252d0210bb44ced9530581ff2
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-07-04 16:52:40 +0900

    利用者一覧表示
    ユーザーログイン機能と同時に実装したため混在の可能性あり

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # get 'users/show'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
     passwords: 'users/passwords'
   }
   root to: 'schedules#index'
+  resources :users, only: [:index, :show]
 end
