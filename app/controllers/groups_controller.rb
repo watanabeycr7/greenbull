cc3a6de532d871a0634fd86a04da2faa836ef29e
31-wtcr <104411739+31-wtcr@users.noreply.github.com>
2022-07-05 14:49:17 +0900

    グループ新規作成と一覧表示

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,22 @@
+class GroupsController < ApplicationController
+
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(group_params)
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
+
+end
